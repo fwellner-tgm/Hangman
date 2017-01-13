21f8b7951da99162f7c5dbbc65ef4d0819cdc053
Florian Wellner <fwellner@student.tgm.ac.at>
2017-01-13 10:26:03 +0100

    hangmanprotocol finished, now needs to be implemented in the server

--- src/Wellner/HangmanProtocol.java
+++ src/Wellner/HangmanProtocol.java
@@ -1,23 +1,41 @@
 package Wellner;
 
-import java.util.Scanner;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class HangmanProtocol {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int tries = 3;
+		ArrayList<String> words = new ArrayList<String>();
+		Random random = new Random();
 
-		String gameWord = "HalloWelt".toUpperCase();
+		try (Stream<String> stream = Files
+				.lines(Paths.get("C:/Users/Florian/OneDrive/Workspace/Hangman/src/Wellner/words.txt"))) {
+			stream.forEach(words::add);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String gameWord = words.get(random.nextInt(words.size())).toUpperCase();
 		StringBuilder masked = new StringBuilder(gameWord.replaceAll(".", "_"));
-
 		while (true) {
-			System.out.println(tries + " tries left: " + masked);
-			String input = sc.nextLine().toUpperCase();
 
-			if (masked.equals(gameWord)) {
-				System.out.println("You win!");
+			if (tries == 0) {
+				System.out.println("You lose!\nThe word was: " + gameWord);
 				break;
 			}
+
+			System.out.println(tries + " tries left: " + masked);
+
+			if (masked.toString().equals(gameWord)) {
+				System.out.println("WIN!");
+				break;
+			}
+
+			String input = sc.nextLine().toUpperCase();
 
 			if (input.length() == 1) {
 				char c = input.charAt(0);
@@ -29,9 +47,16 @@ public class HangmanProtocol {
 
 				}
 
+			} else if (input.length() > 1) {
+				if (input.equals(gameWord)) {
+					System.out.println("Word was: " + gameWord);
+					System.out.println("You Win!");
+					break;
+				} else {
+					tries--;
+				}
 			}
-			
-			// input > 1... tries--;...
+
 		}
 
 	}
